2e27af3d1aeea9a4791e3b45f24a4677cc323c2f
hwr <1092021277@qq.com>
2017-11-17 15:37:02 +0800

    [feat] 增加less配置

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -34,7 +34,18 @@ module.exports = {
                 test: /\.js|jsx$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "less-loader" // compiles Less to CSS
+                }]
+            },
+
         ]
     },
 
